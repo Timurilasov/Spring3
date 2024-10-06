f4af1c2941184ac3a87b51889754989c3339202f
Timurilasov <ilasovtimur08@gmail.com>
2024-10-06 10:49:54 +0300

    Add files via upload

---
+++ Task-7/src/main/java/koschei/models/Egg6.java
@@ -0,0 +1,13 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Egg6 {
+private Needle7 needle7;
+    @Override
+    public String toString() {
+        return ", в яйце иголка " + needle7.toString() ;
+
+    }
+}
